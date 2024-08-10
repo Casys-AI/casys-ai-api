7ef50adf988908f419f14183817a8d65121500c7
superWorldSavior <erpesle@gmail.com>
2024-08-10 21:41:08 +0800

    SMA-11 File uploading, processing, diagram retrieval, JSON processing with button press.

--- main.py
+++ main.py
@@ -5,26 +5,27 @@ import logging
 from logging.handlers import RotatingFileHandler
 from src.infrastructure.config import config
 
+
 def setup_logging(config_log_level: str):
     # Configuration of the main logger
     root_logger = logging.getLogger("uvicorn.error")
     root_logger.setLevel(logging.DEBUG)  # Capture all log levels
-
+    
     # Custom formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s')
-
+    
     # Console handler (INFO and above)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(config_log_level.upper())
     console_handler.setFormatter(formatter)
     root_logger.addHandler(console_handler)
-
+    
     # File handler with rotation (all logs)
     file_handler = RotatingFileHandler("app.log", maxBytes=10 * 1024 * 1024, backupCount=5)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
     root_logger.addHandler(file_handler)
-
+    
     # Adjust log level for uvicorn.access if necessary
     logging.getLogger('uvicorn.access').setLevel(logging.WARNING)
 
@@ -32,10 +33,10 @@ def setup_logging(config_log_level: str):
 if __name__ == "__main__":
     log_level = os.environ.get("LOG_LEVEL", "debug")
     setup_logging(log_level)
-
+    
     logger = logging.getLogger("uvicorn.error")  # Updated line
     logger.info("Starting the application...")
-
+    
     try:
         uvicorn.run(
             "src.adapters.web.api:app",
